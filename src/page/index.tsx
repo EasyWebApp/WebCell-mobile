bed7e6261d0d309d9436b02f41e627609f9ec26a
soecka <twcbnubio@gmail.com>
2025-03-14 00:32:42 +0800

    [add] tailwindcss, eslint config [update] package [polish] layout

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,13 +1,19 @@
-import { createRouter } from 'cell-router';
-import { observable } from 'mobx';
-import { attribute, component, observer } from 'web-cell';
-
 import 'mdui/components/button-icon';
 import 'mdui/components/list';
 import 'mdui/components/list-item';
-import 'mdui/components/navigation-drawer';
-import 'mdui/components/top-app-bar';
-import 'mdui/components/top-app-bar-title';
+import 'mdui/components/top-app-bar.js';
+import 'mdui/components/top-app-bar-title.js';
+import 'mdui/components/navigation-drawer.js';
+import 'mdui/components/layout.js';
+import 'mdui/components/layout-item.js';
+import 'mdui/components/layout-main.js';
+
+import { createRouter } from 'cell-router';
+import type { Layout } from 'mdui/components/layout.js';
+import type { LayoutItem } from 'mdui/components/layout-item.js';
+import type { LayoutMain } from 'mdui/components/layout-main.js';
+import { observable } from 'mobx';
+import { attribute, component, observer } from 'web-cell';
 
 import { renderMode } from '../utility';
 import { CompanyList } from './CompanyList';
@@ -25,14 +31,14 @@ export class PageBox extends HTMLElement {
         const { drawerOpen } = this;
 
         return (
-            <>
-                <mdui-top-app-bar>
+            <mdui-layout className="h-screen bg-white">
+                <mdui-top-app-bar className="flex items-center gap-4 p-4">
                     <mdui-button-icon
                         icon="menu"
                         onClick={() => (this.drawerOpen = !this.drawerOpen)}
                     />
                     <mdui-top-app-bar-title>Anti 996</mdui-top-app-bar-title>
-                    <div className="flex-fill" />
+                    <div className="flex-1" />
                     <mdui-button-icon icon="more_vert" />
                 </mdui-top-app-bar>
 
@@ -47,8 +53,10 @@ export class PageBox extends HTMLElement {
                     </mdui-list>
                 </mdui-navigation-drawer>
 
-                <Route path="" component={CompanyList} />
-            </>
+                <mdui-layout-main>
+                    <Route path="" component={CompanyList} />
+                </mdui-layout-main>
+            </mdui-layout>
         );
     }
 }
